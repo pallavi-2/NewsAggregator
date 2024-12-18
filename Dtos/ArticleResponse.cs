8177711f83ca676fd5960012b1f5ff1099de5200
pallavi-2 <pallavipandey299@gmail.com>
2024-12-18 10:38:42 +0545

    Changed the model for Currents Api

--- Dtos/ArticleResponse.cs
+++ Dtos/ArticleResponse.cs
@@ -2,7 +2,8 @@
 {
     public class ArticleResponse
     {
-        //public int ArticlesId { get; set; }
+        
+        public string ArticleId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime PublishedAt { get; set; }
